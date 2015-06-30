2ee1bfc951f76faf911ed5680ba6bd23c4798f1c
erankush7 <erankush7@gmail.com>
2015-07-01 01:46:14 +0530

    Initial Commit

---
+++ TravelGEO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelGEO
+//
+//  Created by Ankush Sharma on 01/07/15.
+//  Copyright (c) 2015 Ankush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
